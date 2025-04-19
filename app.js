8c38ca5c806ce611b2691dfcbfdb0335746cc7a1
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-20 01:05:16 +0200

    added filter functionality

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 window.addEventListener('load', function () {
     const inputForm = document.querySelector('#input-form');
     const taskList = document.querySelector('#task-list');
+    const filter = document.querySelector('#filter');
+    const sort = document.querySelector('#sort');
     const inputTask = document.querySelector('#input-task');
     const inputDate = document.querySelector('#input-date');
     const tasksArray = [];
 
     function saveTask(task) {
         tasksArray.push(task);
-        console.log(tasksArray);
     }
 
     function deleteTask(index) {
         tasksArray.splice(index, 1);
-        console.log(tasksArray);
     }
 
     function editTask(task, date, index) {
-        tasksArray[index] = {task, date, completed: false};
-        console.log(tasksArray);
+        tasksArray[index] = { task, date, completed: false };
     }
 
     function markComplete(index) {
         tasksArray[index].completed = true;
-        console.log(tasksArray);
     }
 
     inputForm.addEventListener('submit', function (e) {
@@ -76,62 +74,62 @@ window.addEventListener('load', function () {
 
         taskList.appendChild(task_div);
         saveTask({
-            task, 
-            date: new Date(date), 
+            task,
+            date: new Date(date),
             completed: false
         });
-        
+
         inputTask.value = "";
         inputDate.value = "";
     });
 
-    taskList.addEventListener('click', function(e) {
-        if(e.target.classList[0] === 'delete') {
+    taskList.addEventListener('click', function (e) {
+        if (e.target.classList[0] === 'delete') {
             const taskEl = e.target.parentElement.closest('.task');
             deleteTask(parseInt(taskEl.id));
             taskList.removeChild(taskEl);
-        } else if(e.target.classList[0] === 'toggle') {
+        } else if (e.target.classList[0] === 'toggle') {
             const taskEl = e.target.parentElement.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
-            
+
             taskInputs.forEach(taskInput => {
                 taskInput.classList.add('completed');
                 markComplete(taskEl.id);
             });
-            
+
             e.target.style.display = 'none';
             const edit_btn = taskEl.querySelector('.edit');
             edit_btn.style.display = 'none';
-        } else if(e.target.classList[0] === 'edit') {
+        } else if (e.target.classList[0] === 'edit') {
             const taskEl = e.target.parentElement.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
-            
+
             taskInputs.forEach((taskInput, index) => {
                 taskInput.removeAttribute('readonly');
                 taskInput.classList.add('editable');
 
-                if(index === 0) taskInput.focus();
-                if(index === 1) taskInput.type = 'date';
+                if (index === 0) taskInput.focus();
+                if (index === 1) taskInput.type = 'date';
             });
 
             e.target.textContent = 'Save';
             e.target.classList.remove('edit');
             e.target.classList.add('save');
-        } else if(e.target.classList[0] === 'save') {
+        } else if (e.target.classList[0] === 'save') {
             const taskEl = e.target.parentElement.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
 
-            if(!taskInputs[0].value || !taskInputs[1].value) {
+            if (!taskInputs[0].value || !taskInputs[1].value) {
                 alert('Please fill in all the fields!');
                 return;
             };
 
             editTask(taskInputs[0].valuea, new Date(taskInputs[1].value), taskEl.id);
-            
+
             taskInputs.forEach((taskInput, index) => {
                 taskInput.setAttribute('readonly', 'readonly');
                 taskInput.classList.remove('editable');
-                if(index === 1) taskInput.type = 'text';
+                if (index === 1) taskInput.type = 'text';
             });
 
             e.target.textContent = 'Edit';
@@ -139,4 +137,21 @@ window.addEventListener('load', function () {
             e.target.classList.add('edit');
         }
     });
+
+    filter.addEventListener('change', function (e) {
+        const taskNodes = taskList.querySelectorAll('.task');
+        console.log(taskNodes);
+
+        taskNodes.forEach(taskNode => {
+            if (e.target.value === 'all') {
+                taskNode.style.display = 'flex';
+            } else if (e.target.value === 'complete') {
+                const taskEl = taskNode.querySelector('input');
+                taskNode.style.display = taskEl.classList.contains('completed') ? 'flex' : 'none';
+            } else {
+                const taskEl = taskNode.querySelector('input');
+                taskNode.style.display = taskEl.classList.contains('completed') ? 'none' : 'flex';
+            }
+        });
+    })
 });
