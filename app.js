67c9def96f50d89e0666c24daedf658208001c80
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-20 12:53:40 +0200

    added sorted functionality and added dynamic display of list items using array

--- app.js
+++ app.js
@@ -16,11 +16,54 @@ window.addEventListener('load', function () {
     }
 
     function editTask(task, date, index) {
-        tasksArray[index] = { task, date, completed: false };
+        tasksArray[index].task = task;
+        tasksArray[index].date = date;
     }
 
-    function markComplete(index) {
-        tasksArray[index].completed = true;
+    function markAsComplete(index) {
+        tasksArray[index].complete = true;
+    }
+
+    function displayTaskList(tasks) {
+        taskList.innerHTML = tasks.map((task, index) => (
+            `
+                <div id=${index} class="task">
+                    <div class="content">
+                        <input type="text" value=${task.title} class=${task.complete ? "completed" : ""} readonly />
+                        <input type="text" value=${task.date} class=${task.complete ? "completed" : ""} readonly />
+                    </div>
+                    <div class="task-btns">
+                        <button class="toggle" style="display:${task.complete ? "none" : "unset"};">Complete</button>
+                        <button class="edit" style="display:${task.complete ? "none" : "unset"};">Edit</button>
+                        <button class="delete">Delete</button>
+                    </div>
+                </div>
+            `
+        )).join('');
+    }
+
+    function filterTaskList(e) {
+        if (e.target.value === 'all') {
+            displayTaskList(tasksArray);
+        } else if (e.target.value === 'complete') {
+            const toBeDisplayed = tasksArray.filter(task => task.complete === true);
+            displayTaskList(toBeDisplayed);
+        } else {
+            const toBeDisplayed = tasksArray.filter(task => task.complete === false);
+            displayTaskList(toBeDisplayed);
+        }
+    }
+
+    function sortTaskList(e) {
+        if (e.target.value === 'none') {
+            displayTaskList(tasksArray);
+        } else if (e.target.value === 'ascending') {
+            const ascending = [...tasksArray].sort((a, b) => new Date(a.date) - new Date(b.date));
+            displayTaskList(ascending);
+        } else {
+            const descending = [...tasksArray].sort((a, b) => new Date(b.date) - new Date(a.date));
+            displayTaskList(descending);
+        }
     }
 
     inputForm.addEventListener('submit', function (e) {
@@ -28,80 +71,36 @@ window.addEventListener('load', function () {
         const task = inputTask.value;
         const date = inputDate.value;
 
-        const task_div = document.createElement('div');
-        task_div.classList.add('task');
-
-        const content_div = document.createElement('div');
-        content_div.classList.add('content');
-
-        const task_el = document.createElement('input');
-        task_el.type = 'text';
-        task_el.value = task;
-        task_el.setAttribute('readonly', 'readonly');
-        task_el.setAttribute('required', 'required');
-
-        const date_el = document.createElement('input');
-        date_el.type = 'text';
-        date_el.value = date;
-        date_el.setAttribute('readonly', 'readonly');
-        date_el.setAttribute('required', 'required');
-
-        content_div.appendChild(task_el);
-        content_div.appendChild(date_el);
-
-        const btns_div = document.createElement('div');
-        btns_div.classList.add('task-btns');
-
-        const complete_btn = document.createElement('button');
-        complete_btn.classList.add('toggle');
-        complete_btn.textContent = 'Complete';
-
-        const edit_btn = document.createElement('button');
-        edit_btn.classList.add('edit');
-        edit_btn.textContent = 'Edit';
-
-        const delete_btn = document.createElement('button');
-        delete_btn.classList.add('delete');
-        delete_btn.textContent = 'Delete';
-
-        btns_div.appendChild(complete_btn);
-        btns_div.appendChild(edit_btn);
-        btns_div.appendChild(delete_btn);
-
-        task_div.appendChild(content_div);
-        task_div.appendChild(btns_div);
-        task_div.id = tasksArray.length.toString();
-
-        taskList.appendChild(task_div);
         saveTask({
-            task,
-            date: new Date(date),
-            completed: false
+            title: task,
+            date,
+            complete: false
         });
 
         inputTask.value = "";
         inputDate.value = "";
+
+        displayTaskList(tasksArray);
     });
 
     taskList.addEventListener('click', function (e) {
         if (e.target.classList[0] === 'delete') {
-            const taskEl = e.target.parentElement.closest('.task');
+            const taskEl = e.target.closest('.task');
             deleteTask(parseInt(taskEl.id));
             taskList.removeChild(taskEl);
         } else if (e.target.classList[0] === 'toggle') {
-            const taskEl = e.target.parentElement.closest('.task');
+            const taskEl = e.target.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
+            const taskBtns = taskEl.querySelectorAll('button');
 
-            taskInputs.forEach(taskInput => {
-                taskInput.classList.add('completed');
-                markComplete(taskEl.id);
-            });
+            taskInputs.forEach(taskInput => taskInput.classList.add('completed'));
 
-            e.target.style.display = 'none';
-            const edit_btn = taskEl.querySelector('.edit');
-            edit_btn.style.display = 'none';
+            markAsComplete(taskEl.id);
+
+            taskBtns[0].style.display = 'none';
+            taskBtns[1].style.display = 'none';
         } else if (e.target.classList[0] === 'edit') {
-            const taskEl = e.target.parentElement.closest('.task');
+            const taskEl = e.target.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
 
             taskInputs.forEach((taskInput, index) => {
@@ -116,7 +115,7 @@ window.addEventListener('load', function () {
             e.target.classList.remove('edit');
             e.target.classList.add('save');
         } else if (e.target.classList[0] === 'save') {
-            const taskEl = e.target.parentElement.closest('.task');
+            const taskEl = e.target.closest('.task');
             const taskInputs = taskEl.querySelectorAll('input');
 
             if (!taskInputs[0].value || !taskInputs[1].value) {
@@ -124,7 +123,7 @@ window.addEventListener('load', function () {
                 return;
             };
 
-            editTask(taskInputs[0].valuea, new Date(taskInputs[1].value), taskEl.id);
+            editTask(taskInputs[0].value, taskInputs[1].value, taskEl.id);
 
             taskInputs.forEach((taskInput, index) => {
                 taskInput.setAttribute('readonly', 'readonly');
@@ -138,20 +137,7 @@ window.addEventListener('load', function () {
         }
     });
 
-    filter.addEventListener('change', function (e) {
-        const taskNodes = taskList.querySelectorAll('.task');
-        console.log(taskNodes);
+    filter.addEventListener('change', filterTaskList);
 
-        taskNodes.forEach(taskNode => {
-            if (e.target.value === 'all') {
-                taskNode.style.display = 'flex';
-            } else if (e.target.value === 'complete') {
-                const taskEl = taskNode.querySelector('input');
-                taskNode.style.display = taskEl.classList.contains('completed') ? 'flex' : 'none';
-            } else {
-                const taskEl = taskNode.querySelector('input');
-                taskNode.style.display = taskEl.classList.contains('completed') ? 'none' : 'flex';
-            }
-        });
-    })
+    sort.addEventListener('change', sortTaskList);
 });
